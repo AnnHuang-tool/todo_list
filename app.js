70721ee732cb417e18533b605963e1093089055d
Ann Huang <s52561@yahoo.com.tw>
2020-07-21 15:30:05 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
@@ -16,9 +17,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('123')
+  res.render('index')
 })
 
 // 設定 port 3000
