81e71ed50664bf6325b15c2fae2abaf7e7eb5394
Ann Huang <s52561@yahoo.com.tw>
2020-07-21 17:17:57 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
+// 引用 body-parser
+const bodyParser = require('body-parser')
 const Todo = require('./models/todo') // 載入 Todo model
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -22,6 +23,10 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 
 app.get('/', (req, res) => {
@@ -29,6 +34,18 @@ app.get('/', (req, res) => {
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // app.get('/', (req, res) => {
