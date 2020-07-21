fb45630ef4bd4f74bf8c0701204a0f8875c8cc82
Ann Huang <s52561@yahoo.com.tw>
2020-07-21 17:39:28 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -36,11 +36,12 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理
 })
 
-
+// 新增
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
+// 瀏覽
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -49,6 +50,15 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 編輯
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+// Create 功能：資料庫新增資料
 app.post('/todos', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
   return Todo.create({ name })     // 存入資料庫
@@ -56,6 +66,18 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Update 功能：資料庫修改特定 todo 的資料
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
 // app.get('/', (req, res) => {
 //   res.render('index')
 // })
