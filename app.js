2f5f7a6bc979cd9335d685ee586cdcaa1941a6ea
Ann Huang <s52561@yahoo.com.tw>
2020-07-21 15:39:00 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
+const Todo = require('./models/todo') // 載入 Todo model
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
@@ -22,9 +23,17 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // 設定首頁路由
+
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
+
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
 
 // 設定 port 3000
 app.listen(3000, () => {
