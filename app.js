cbcfb4f2051e8ac9d1b964c6940d0c71affb1a6b
Ann Huang <s52561@yahoo.com.tw>
2020-07-31 14:16:36 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -11,20 +11,8 @@ const methodOverride = require('method-override')
 
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose')
 
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const Todo = require('../todo') // 載入 todo model
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+// mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+// const db = mongoose.connection
+
+const db = require('../../config/mongoose')
+
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
+// db.once('open', () => {
+//   console.log('mongodb connected!')
+// })
 
 db.once('open', () => {
   console.log('mongodb connected!')
