27018cbfc319096e8e3169380f5d3996cbaa2b55
Ann Huang <s52561@yahoo.com.tw>
2020-08-19 14:52:53 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const session = require('express-session')
 const app = express()
 // app.js
 // 如果在 Heroku 環境則使用 process.env.PORT
@@ -31,6 +32,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // 將 request 導入路由器
 app.use(routes)
--- models/user.js
+++ models/user.js
@@ -10,14 +10,14 @@ const userSchema = new Schema({
     type: String, // 資料型別是字串
     required: true, // 這是個必填欄位
   },
-  password{
-  type: String, // 資料型別是字串
-  required: true, // 這是個必填欄位
-},
-  creaedAt: {
-  type: Date,
-  defult: Date.now
-}
+  password: {
+    type: String, // 資料型別是字串
+    required: true, // 這是個必填欄位
+  },
+  createdAt: {
+    type: Date,
+    defult: Date.now
+  }
 
 })
 module.exports = mongoose.model('User', userSchema)
