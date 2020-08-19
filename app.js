c05e8533fa2a4380c28e67845af625306f5d2097
Ann Huang <s52561@yahoo.com.tw>
2020-08-19 15:38:05 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 // engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
